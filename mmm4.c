2fda956195fcebf2956da9a570e2ad8f71563383
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-01-21 17:02:43 +0530

    Create mmm4.c

---
+++ mmm4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+clrscr();
+printf("enter the value a");
+scanf("%c",&a);
+if(a==1<=100000)
+printf("Not");
+else
+printf("Alphabet");
+getch();
+}
