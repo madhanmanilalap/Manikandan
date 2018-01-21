c148a07bd3cf1121d95faf83b61d2d57bda18ba2
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-01-21 15:21:09 +0530

    Create mmm.c

---
+++ mmm.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+int a=7;
+clrscr();
+printf("%d",a);
+if(a<=7)
+{
+printf("Positive");
+if(a=0)
+{
+printf("Zero");
+else
+printf("Negative");
+}}
+getch();
+}
