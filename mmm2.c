bc02108e395ed9932d463d5b6c1d506d149a054f
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-01-21 16:14:11 +0530

    Create mmm2.c

---
+++ mmm2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("Enter The Value a");
+scanf("%d",&a);
+if(a%2==0)
+printf("Even Number");
+else
+printf("Odd number");
+getch();
+}
